e6f3533b2c5bfbc0e3744ebef9c3f02c03b615a5
Kayo-Nagai117 <kayonagai115@gmail.com>
2026-05-12 03:00:24 -0300

    Atualiza├º├úo autom├ítica

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
+
+
+
+
 function _toggleMode() {
 	const html = document.documentElement
 	html.classList.toggle("light")
 
-// if(html.classList.contains("light")){
-// html.classList.remove("light")
-// } else{
-// html.classList.add("light")
-// }
-// }
 
 //pegar a taag img
 const img = document.querySelector("#profile img");
@@ -15,9 +13,12 @@ const img = document.querySelector("#profile img");
 if (html.classList.contains("light")) {
 	// se tiver light mode, adicionar a imagem light
 	img.setAttribute("src", "./assets/Açai_da_Mercia_light.png");
+	toggleButton.style.right = "0";
 } else {
 	// se tiver sem light mode, manter a imagem normal
 	img.setAttribute("src", "./assets/Açai_da_Mercia.jpg");
+	toggleButton.style.right = "auto";
+	toggleButton.style.left = "0";
 }
 
 }
